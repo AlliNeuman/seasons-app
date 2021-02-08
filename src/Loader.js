1f0a67d237a0c7b1c320c05dec30ec67ff41fd04
Alli Neuman <alli.neuman@gmail.com>
2021-02-07 23:43:28 -0600

    add all functionality of components and styling

---
+++ src/Loader.js
@@ -0,0 +1,15 @@
+import './Loader.css';
+import React from 'react';
+
+const Loader = (props) => {
+return (
+    <div class="ui active dimmer">
+    <div class="ui huge text loader">{props.message}</div>
+  </div>
+)
+}
+
+Loader.defaultProps = {
+    message: 'Loading...'
+}
+export default Loader;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import SeasonDisplay from './SeasonDisplay';
+import Loader from './Loader';
+
+class App extends React.Component {
+    // set initial state for new instance of app
+    state = {
+        lat: null,
+        errorMessage: ''
+    };
+
+    componentDidMount() {
+        // Get coordinates from browser geolocation
+        // Update state object with this.setState
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => this.setState({ lat: position.coords.latitude }),
+            (err) => this.setState({ errorMessage: err.message})
+        );
+    }
+    componentDidUpdate() {
+        console.log('My component was just updated - it rerendered!');
+    }
+    //helper to check what content to render
+    renderContent () {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+        if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat} />;
+        }
+        return <Loader message="Please accept location request"/>;
+    }
+    render() {
+        return (
+            <div>
+                {this.renderContent()}
+            </div>
+        )
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App />, document.getElementById("root")
+)
